e62660cae5bb31911beabcb4d549c0ffcd9d2f17
Vaelastraszz <romain.lejeun@gmail.com>
2024-07-08 16:22:03 +0200

    adding logical_or function
    checking if they have any order with a unit price over 50 euros

--- analyses/top_ten_customers_unit_price_over_fifty.sql
+++ analyses/top_ten_customers_unit_price_over_fifty.sql
@@ -3,7 +3,8 @@ with base_cte as (
         dcust.sk_customer as sk_customer,
         dcust.dsc_name as customer_name,
         round(sum(fct.mtr_total_amount_net), 2) as mtr_total_amount_net,
-        logical_and(fct.mtr_unit_price > 50) as all_unit_price_above_fifty
+        logical_and(fct.mtr_unit_price > 50) as all_unit_price_above_fifty,
+        logical_or(fct.mtr_unit_price > 50) as any_unit_price_above_fifty
     from {{ ref("fct_purchase_history") }} as fct
     left join {{ ref("dim_customers") }} as dcust on dcust.sk_customer = fct.sk_customer_id
     group by 1, 2
